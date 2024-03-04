0f5457379a2b2f7114272b4b0c349ca975b9a36f
fernandes-vinicius <viniciusfernandes6991@gmail.com>
2024-03-04 16:44:42 -0300

    chore: add utfs.io hostname in next config

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,17 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = {
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'utfs.io',
+        port: '',
+        pathname: '/**',
+      },
+    ],
+    // Disable next.js image optimization for all images on production for reducing costs with Vercel Plan
+    unoptimized: process.env.NODE_ENV === 'production',
+  },
+}
 
 export default nextConfig
