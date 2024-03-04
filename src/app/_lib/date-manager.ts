4d321daf3ac6c81cc2c2a7b12f9776b382b8769a
fernandes-vinicius <viniciusfernandes6991@gmail.com>
2024-03-04 13:40:30 -0300

    feat: add date-manager util using date-fns lib

---
+++ src/app/_lib/date-manager.ts
@@ -0,0 +1,26 @@
+import * as dateFns from 'date-fns'
+import { ptBR } from 'date-fns/locale'
+
+type DateType = string | number | Date
+
+export abstract class DateManger {
+  abstract format: (format?: string) => string
+}
+
+class DateFnsDateManger implements DateManger {
+  date: DateType
+  locale
+
+  constructor(date: DateType) {
+    this.date = date
+    this.locale = ptBR
+  }
+
+  format(format = 'dd/MM/yyyy') {
+    return dateFns.format(this.date, format, { locale: this.locale })
+  }
+}
+
+export const dateManager = (date: DateType = new Date()): DateManger => {
+  return new DateFnsDateManger(date)
+}
