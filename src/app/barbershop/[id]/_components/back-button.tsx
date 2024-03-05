17d543482f10e093aeb07cf6fb9be27b5ff8f925
fernandes-vinicius <viniciusfernandes6991@gmail.com>
2024-03-05 10:34:45 -0300

    feat: add BackButton on barbershop page

---
+++ src/app/barbershop/[id]/_components/back-button.tsx
@@ -0,0 +1,27 @@
+'use client'
+
+import { useRouter } from 'next/navigation'
+
+import { ChevronLeftIcon } from 'lucide-react'
+
+import { Button } from '@/app/_components/ui/button'
+
+export function BackButton() {
+  const router = useRouter()
+
+  function handleBack() {
+    router.back()
+  }
+
+  return (
+    <Button
+      aria-label="Go back"
+      type="button"
+      variant="secondary"
+      size="icon"
+      onClick={handleBack}
+    >
+      <ChevronLeftIcon className="size-5" />
+    </Button>
+  )
+}
