f26c44b86adfabd14880837e1b1fad5a5bbd1250
fernandes-vinicius <viniciusfernandes6991@gmail.com>
2024-03-05 10:49:52 -0300

    feat: add util function to format cents to currency value

--- src/app/_lib/utils.ts
+++ src/app/_lib/utils.ts
@@ -4,3 +4,12 @@ import { twMerge } from 'tailwind-merge'
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs))
 }
+
+export function formatCentsToCurrency(cents: number) {
+  const amount = cents / 100
+
+  return new Intl.NumberFormat('pt-BR', {
+    style: 'currency',
+    currency: 'BRL',
+  }).format(amount)
+}
